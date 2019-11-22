a3abe31ff7d2fefbce0f248a268a81102a8ce455
Stefano Nerozzi <stefano.nerozzi@utexas.edu>
2019-11-22 13:20:40 -0600

    some debugging

--- rollmean.py
+++ rollmean.py
@@ -17,8 +17,8 @@ avg_dist = 2
 
 infile = 'cl_5x5.csv'
 outfile = 'cl_5x5_gauss_1.5s.csv'
-in_dir = '/home/stefano/Boulder_Halos/GRS/elements-v1/unsmoothed/5x5/'
-out_dir = '/home/stefano/Boulder_Halos/GRS/elements-v1/processed/5x5/'
+in_dir = '/mnt/d/Dropbox/MARS/GRS/elements-v1/unsmoothed/5x5/'
+out_dir = '/mnt/d/Dropbox/MARS/GRS/elements-v1/processed2/5x5/'
 outfile = out_dir+outfile
 infile = in_dir+infile
 
@@ -51,15 +51,15 @@ def main():
   datalst = np.loadtxt(infile,delimiter=',')
 
   # Put input data into a grid
-  latcells = 180/csize_in
-  loncells = 360/csize_in
-  datagrid = np.zeros((latcells,loncells))
+  latcells_in = int(180/csize_in) # number of input latitude cells
+  loncells_in = int(360/csize_in) # number of input longitude cells
+  datagrid = np.zeros((latcells_in,loncells_in))
   for k in range(len(datalst)):
     # Funky indexing to deal with order of points in file
     # This will break if ordering of points in input file changes
     # Probably better to actually read lat and lon and use that
-    i = (latcells-1)-(k//loncells)
-    j = k%loncells
+    i = (latcells_in-1)-(k//loncells_in)
+    j = k%loncells_in
     datagrid[i,j] = datalst[k,2]
 
   # Generate lat and lon arrays
@@ -76,19 +76,21 @@ def main():
   # [ ..., ..., ...., ...]
   #
   # These are used for haversine distance calculation
-  lonl = np.matrix(np.linspace(csize_in/2, 360-csize_in/2, loncells))
-  latl = np.matrix(np.linspace(90-csize_in/2, -90+csize_in/2, latcells))
+  lonl = np.matrix(np.linspace(csize_in/2, 360-csize_in/2, loncells_in))
+  latl = np.matrix(np.linspace(90-csize_in/2, -90+csize_in/2, latcells_in))
   lon = np.dot(np.ones((datagrid.shape[0],1)), lonl)
   lat = np.dot(np.transpose(latl),np.ones((1,datagrid.shape[1])))
 
-  # Make output grid
-  outgrid = np.zeros((180/csize_out, 360/csize_out))
-  olon = np.linspace(csize_out/2, 360-csize_out/2, 360/csize_out)
-  olat = np.linspace(90-360/csize_out, -90+360/csize_out, 180/csize_out)
+  # Make n*n degree output grid
+  latcells_out = int(180/csize_out) # number of output latitude cells
+  loncells_out = int(360/csize_out) # number of output longitude cells
+  outgrid = np.zeros((latcells_out, loncells_out))
+  olon = np.linspace(csize_out/2, 360-csize_out/2, loncells_out)
+  olat = np.linspace(90-csize_out/2, -90+csize_out/2, latcells_out)
 
   # Do averaging, iterating over output grid
-  for i in range(180/csize_out):
-    for j in range(360/csize_out):
+  for i in range(latcells_out):
+    for j in range(loncells_out):
       # Haversine distance from each point to rest of grid
       dist = havs(olat[i], olon[j], lat, lon, marsradius)
       # Calculate weights for average based on gaussian function
@@ -96,22 +98,21 @@ def main():
       # Mask out points and weights outside search radius
       mask = dist < avg_dist*sigma
       subpts = datagrid[mask]
-      print('weights: ' + str(weights))
       weights = weights[mask]
-      print('weights masked: ' + str(weights))
       # Turn 9999.999 into NaN
       subpts[subpts == 9999.999] = np.NaN
       # Multiply values with their weights
       subpts = np.multiply(weights, subpts)
       # Calculate average ignoring NaNs
       outgrid[i,j] = np.nanmean(subpts)
+      print(str(olat[i]) + " " + str(olon[j]) + " " + str(np.nansum(len(weights))) + " " + str(np.nansum(weights)))
       # Propagate errors (averaging improves errors by 1/sqrt(N averaged samples))
       #outgrid[i,j,1:] = outgrid[i,j,1:] / np.sqrt(np.nansum(weights))
 
   # Write out text file in similar format
   with open(outfile, "w") as f:
-    for i in range(180/csize_out):
-      for j in range(360/csize_out):
+    for i in range(latcells_out):
+      for j in range(loncells_out):
         f.write("{},{},{}\n".format(olat[i], olon[j], outgrid[i,j]))
    
 main()
